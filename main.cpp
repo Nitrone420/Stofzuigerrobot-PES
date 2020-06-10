c703b44a43e539b10dfd64fe56bf7b0222041be8
Karub97 <66719778+Karub97@users.noreply.github.com>
2020-06-10 10:04:04 +0200

    inputs

--- main.cpp
+++ main.cpp
@@ -1,3 +1,33 @@
 //Hello World!
 //PES Groep 3
 //Dit is een test
+
+#include "mbed.h"
+
+// I/O lijst
+
+// Inputs
+    InterruptIn start_Button(PA_5);                     // Knop om de robotsofzuiger te starten.
+
+    InterruptIn US_LV(PA_6);                            // Ultrasoonsensor die links voorin gemonteerd is (zie technische tekeningen).
+    InterruptIn US_RV(PA_6);                            // Ultrasoonsensor die rechts voorin gemonteerd is (zie technische tekeningen).
+
+    InterruptIn US_LA(PA_7);                            // Ultrasoonsensor die links achterin gemonteerd is (zie technische tekeningen).
+    InterruptIn US_RA(PB_6);                            // Ultrasoonsensor die rechts voorin gemonteerd is (zie technische tekeningen).
+
+    InterruptIn IR_LV(PC_7);                                                                               
+    InterruptIn IR_RV(PA_9);
+
+    InterruptIn IR_LA(PA_8);
+    InterruptIn IR_RA(PB_10);
+
+
+int main(){
+
+    
+    
+
+    while(true){
+        
+    }
+}
