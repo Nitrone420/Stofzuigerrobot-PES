08406e8b7a544aa4d2f4379ef5f40d48e6d05d0b
Karub97 <66719778+Karub97@users.noreply.github.com>
2020-06-10 10:41:29 +0200

    STATE en switch bij toegevoegd

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 // I/O lijst
 
 // Inputs
-InterruptIn start_Button(PA_5);                     // Knop om de robotsofzuiger te starten.
+InterruptIn start_Button(PA_5);                    // Knop om de robotsofzuiger te starten.
 
 InterruptIn US_V(PA_6);                            // Ultrasoonsensor die links voorin gemonteerd is (zie technische tekeningen).
 InterruptIn US_A(PA_7);                            // Ultrasoonsensor die rechts voorin gemonteerd is (zie technische tekeningen).
@@ -24,10 +24,98 @@ DigitalOut AN(PB_4);
 
 int main(){
 
-    
-    
+    enum state{OFF, WACHT, RIJDEN, ONTWIJKEN};
+
+    bool OFF_first = true;
+    bool WACHT_first = true;
+
+    bool RIJDEN_first = true;
+    bool ONTWIJKEN_first = true;
 
     while(true){
-        
+
+        switch(){
+
+            case OFF:
+
+                //entry
+                if(){
+
+                }
+            
+                //do
+                if(){
+
+                }
+
+                // exit
+                if(){
+
+                }
+
+            break;
+
+            case WACHT:
+
+                //entry
+                if(){
+
+                }
+            
+                //do
+                if(){
+
+                }
+
+                // exit
+                if(){
+
+                }
+
+            break;
+
+            case RIJDEN:
+
+                //entry
+                if(){
+
+                }
+            
+                //do
+                if(){
+
+                }
+
+                // exit
+                if(){
+
+                }
+
+            break;
+
+            case ONTWIJKEN:
+
+                //entry
+                if(){
+
+                }
+            
+                //do
+                if(){
+
+                }
+
+                // exit
+                if(){
+
+                }
+
+            break;
+
+            default:
+                printf("Invalid state ")
+            break;
+        }
     }
+    return 0;
 }
