7d5005119c8bb56f2b97e9c33aca5da33b34dcc3
Karub97 <66719778+Karub97@users.noreply.github.com>
2020-06-10 10:22:03 +0200

    Input/output aangepast

--- main.cpp
+++ main.cpp
@@ -3,23 +3,23 @@
 //Dit is een test
 
 #include "mbed.h"
+#include "platform/mbed_thread.h"
 
 // I/O lijst
 
 // Inputs
-    InterruptIn start_Button(PA_5);                     // Knop om de robotsofzuiger te starten.
+InterruptIn start_Button(PA_5);                     // Knop om de robotsofzuiger te starten.
 
-    InterruptIn US_LV(PA_6);                            // Ultrasoonsensor die links voorin gemonteerd is (zie technische tekeningen).
-    InterruptIn US_RV(PA_6);                            // Ultrasoonsensor die rechts voorin gemonteerd is (zie technische tekeningen).
+InterruptIn US_V(PA_6);                            // Ultrasoonsensor die links voorin gemonteerd is (zie technische tekeningen).
+InterruptIn US_A(PA_7);                            // Ultrasoonsensor die rechts voorin gemonteerd is (zie technische tekeningen).
 
-    InterruptIn US_LA(PA_7);                            // Ultrasoonsensor die links achterin gemonteerd is (zie technische tekeningen).
-    InterruptIn US_RA(PB_6);                            // Ultrasoonsensor die rechts voorin gemonteerd is (zie technische tekeningen).
+InterruptIn IR_V(PB_6);                            // Infraroodsensor die links voorin gemonteerd is (zie technische tekeningen).                                                                  
+InterruptIn IR_A(PC_7);                            // Infraroodsensor die rechts voorin gemonteerd is (zie technische tekeningen).
 
-    InterruptIn IR_LV(PC_7);                                                                               
-    InterruptIn IR_RV(PA_9);
-
-    InterruptIn IR_LA(PA_8);
-    InterruptIn IR_RA(PB_10);
+// Outputs
+DigitalOut M_Links(PA_8);
+DigitalOut M_rechts(PB_10);
+DigitalOut AN(PB_4);
 
 
 int main(){
